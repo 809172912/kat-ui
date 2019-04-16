ab9092f729802b5135af2f1782d3e27f3963f38e
809172912 <809172912@qq.com>
2019-04-16 11:54:11 +0800

    [开发] 实例文档引用本kat-ui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './examples/views/App.vue'
 import router from './examples/router'
 import store from './store'
+import demoBlock from './examples/components/demo-block'
+import katUI from './packages/components/index'
+import './packages/default-theme/index.css'
 
 Vue.config.productionTip = false
+
+Vue.component('demo-block', demoBlock)
+Vue.use(katUI)
 
 new Vue({
   router,
